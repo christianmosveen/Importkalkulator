345559e0ee4e59ed210619941a7c40ba18b86313
Christian H. Mosveen <neypalm@gmail.com>
2012-08-17 14:09:47 +0200

    Koblet slidere til verdier og utregning av avgift (uten bruksfradrag)

--- Importkalkulator/EngangsavgiftViewController.h
+++ Importkalkulator/EngangsavgiftViewController.h
@@ -1,13 +1,32 @@
 #import <UIKit/UIKit.h>
+#import "KalkulerEngangsavgift.h"
 
 @interface EngangsavgiftViewController : UIViewController
 
+@property (nonatomic, retain) KalkulerEngangsavgift *kalkuler;
+@property int vekt;
+@property int effekt;
+@property int co2;
+@property int nox;
+@property float avgift;
+
 @property (nonatomic) IBOutlet UITextField *navn;
-@property ( nonatomic) IBOutlet UILabel *vekt;
-@property ( nonatomic) IBOutlet UILabel *effekt;
-@property ( nonatomic) IBOutlet UILabel *co2;
-@property ( nonatomic) IBOutlet UILabel *nox;
+@property (nonatomic) IBOutlet UISlider *vektSlider;
+@property (nonatomic) IBOutlet UISlider *effektSlider;
+@property (nonatomic) IBOutlet UISlider *co2Slider;
+@property (nonatomic) IBOutlet UISlider *noxSlider;
+
+@property (nonatomic) IBOutlet UILabel *vektLabel;
+@property (nonatomic) IBOutlet UILabel *effektLabel;
+@property (nonatomic) IBOutlet UILabel *co2Label;
+@property (nonatomic) IBOutlet UILabel *noxLabel;
+@property (nonatomic) IBOutlet UILabel *avgiftLabel;
+
+- (void)kalkulerAvgift;
 
 - (IBAction)vektEndret:(id)sender;
+- (IBAction)effektEndret:(id)sender;
+- (IBAction)co2Endret:(id)sender;
+- (IBAction)noxEndret:(id)sender;
 
 @end
--- Importkalkulator/EngangsavgiftViewController.m
+++ Importkalkulator/EngangsavgiftViewController.m
@@ -2,17 +2,32 @@
 
 @implementation EngangsavgiftViewController
 
-@synthesize navn;
+@synthesize kalkuler;
 @synthesize vekt;
 @synthesize effekt;
 @synthesize co2;
 @synthesize nox;
+@synthesize avgift;
+
+@synthesize navn;
+@synthesize avgiftLabel;
+
+@synthesize vektSlider;
+@synthesize effektSlider;
+@synthesize co2Slider;
+@synthesize noxSlider;
+
+@synthesize vektLabel;
+@synthesize effektLabel;
+@synthesize co2Label;
+@synthesize noxLabel;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        kalkuler = [[KalkulerEngangsavgift alloc] init];
+        
     }
     return self;
 }
@@ -29,18 +44,38 @@
 
 - (void)viewDidLoad
 {
+    vekt = 1300;
+    effekt = 77;
+    co2 = 119;
+    nox = 60;
+    
+    vektSlider.value = vekt;
+    effektSlider.value = effekt;
+    co2Slider.value = co2;
+    noxSlider.value = nox;
+    
+    vektLabel.text = [[NSString alloc] initWithFormat:@"%d", vekt];
+    effektLabel.text = [[NSString alloc] initWithFormat:@"%d", effekt];
+    co2Label.text = [[NSString alloc] initWithFormat:@"%d", co2];
+    noxLabel.text = [[NSString alloc] initWithFormat:@"%d", nox];
+    
+    [self kalkulerAvgift];
+    
     [super viewDidLoad];
 }
 
 - (void)viewDidUnload
 {
-    [self setVekt:nil];
-    [self setEffekt:nil];
-    [self setCo2:nil];
-    [self setNox:nil];
+    [self setVektLabel:nil];
+    [self setEffektLabel:nil];
+    [self setCo2Label:nil];
+    [self setNoxLabel:nil];
+    [self setAvgiftLabel:nil];
+    [self setVektSlider:nil];
+    [self setEffektSlider:nil];
+    [self setCo2Slider:nil];
+    [self setNoxSlider:nil];
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -50,9 +85,39 @@
 }
 
 
-- (IBAction)vektEndret:(id)sender
-{
-    
+- (IBAction)vektEndret:(id)sender {
+    vekt = (int)(vektSlider.value+0.5f);
+    NSString *vektString = [[NSString alloc] initWithFormat:@"%d", vekt];
+    vektLabel.text = vektString;
+    [self kalkulerAvgift];
 }
+
+- (IBAction)effektEndret:(id)sender {
+    effekt = (int)(effektSlider.value+0.5f);
+    NSString *effektString = [[NSString alloc] initWithFormat:@"%d", effekt];
+    effektLabel.text = effektString;
+    [self kalkulerAvgift];
+}
+
+- (IBAction)co2Endret:(id)sender {
+    co2 = (int)(co2Slider.value+0.5f);
+    NSString *co2String = [[NSString alloc] initWithFormat:@"%d", co2];
+    co2Label.text = co2String;
+    [self kalkulerAvgift];
+}
+
+- (IBAction)noxEndret:(id)sender {
+    nox = (int)(noxSlider.value+0.5f);
+    NSString *noxString = [[NSString alloc] initWithFormat:@"%d", nox];
+    noxLabel.text = noxString;
+    [self kalkulerAvgift];
+}
+
+- (void)kalkulerAvgift {
+    avgift = [kalkuler avgiftMedVekt:vekt effekt:effekt nox:nox co2:co2];
+    NSString *avgiftString = [[NSString alloc] initWithFormat:@"%d", (int)(avgift+0.5f)];
+    avgiftLabel.text = avgiftString;
+}
+
 
 @end
